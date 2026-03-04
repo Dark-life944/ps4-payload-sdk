dc79c9141bac1b22718c374132a73d16f1a0a58f
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2026-03-04 03:14:45 +0100

    Update main.c

--- examples/ps4-backup/source/main.c
+++ examples/ps4-backup/source/main.c
@@ -1,34 +1,52 @@
-#include <ps4.h>
+#include "ps4.h"
 
-typedef void (*png_read_info_t)(void*, void*);
-png_read_info_t orig_png_read_info;
+#define debug(sock, format, ...)                    \
+    do {                                            \
+        char buffer[512];                           \
+        int size = sprintf(buffer, format, ##__VA_ARGS__); \
+        sceNetSend(sock, buffer, size, 0);          \
+    } while(0)
 
-void hook_png_read_info(void* png_ptr, void* info_ptr)
-{
-    printf("png_read_info called! png_ptr=%p info_ptr=%p\n", png_ptr, info_ptr);
-    orig_png_read_info(png_ptr, info_ptr);
-}
+int _main(void) {
 
-void _main(void)
-{
+    // Initialize
     initKernel();
     initLibc();
     initNetwork();
-    initPthread();
 
-    // testcase 
-    void *addr = (void*)resolve_sym("libScePngDec.sprx", "png_read_info");
+    char socketName[] = "debug";
 
-    if(addr)
-    {
-        orig_png_read_info = (png_read_info_t)install_hook(addr, hook_png_read_info);
-        printf("hook installed for png_read_info at %p\n", addr);
+    struct sockaddr_in server;
+    server.sin_len = sizeof(server);
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = IP(192, 168, 90, 137);
+    server.sin_port = sceNetHtons(9023);
+    memset(server.sin_zero, 0, sizeof(server.sin_zero));
+
+    int sock = sceNetSocket(socketName, AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+        return 0;
+
+    if (sceNetConnect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        sceNetSocketClose(sock);
+        return 0;
     }
-    else
-    {
-        printf("png_read_info not found\n");
-    }
 
-    // keep payload alive
-    while(1) sceKernelSleep(1);
+    // Print PID
+    debug(sock, "PID: %d\n", syscall(20));
+
+    // ---- SCTP TEST ----
+    int sctp = sceNetSocket("sctp_test", AF_INET, SOCK_STREAM, IPPROTO_SCTP);
+
+    if (sctp < 0) {
+        debug(sock, "SCTP not supported (error: %d)\n", sctp);
+    } else {
+        debug(sock, "SCTP supported\n");
+        sceNetSocketClose(sctp);
+    }
+    // -------------------
+
+    sceNetSocketClose(sock);
+
+    return 0;
 }
