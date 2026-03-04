699e327606a1541d69f3c3588c86f43920476f2d
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2026-03-04 15:02:43 +0100

    Update main.c

--- examples/ps4-backup/source/main.c
+++ examples/ps4-backup/source/main.c
@@ -1,7 +1,7 @@
 #include "ps4.h"
 
-#ifndef IPPROTO_SCTP
-#define IPPROTO_SCTP 132
+#ifndef IPPROTO_ICMPV6
+#define IPPROTO_ICMPV6 58
 #endif
 
 #define debug(sock, format, ...)                    \
@@ -13,7 +13,6 @@
 
 int _main(void) {
 
-    // Initialize
     initKernel();
     initLibc();
     initNetwork();
@@ -23,34 +22,77 @@ int _main(void) {
     struct sockaddr_in server;
     server.sin_len = sizeof(server);
     server.sin_family = AF_INET;
-    server.sin_addr.s_addr = IP(192, 168, 90, 137);
+    server.sin_addr.s_addr = IP(192, 168, 248, 159);
     server.sin_port = sceNetHtons(9023);
     memset(server.sin_zero, 0, sizeof(server.sin_zero));
 
-    int sock = sceNetSocket(socketName, AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
+    int debug_sock = sceNetSocket(socketName, AF_INET, SOCK_STREAM, 0);
+    if (debug_sock < 0)
         return 0;
 
-    if (sceNetConnect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        sceNetSocketClose(sock);
+    if (sceNetConnect(debug_sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        sceNetSocketClose(debug_sock);
         return 0;
     }
 
-    // Print PID
-    debug(sock, "PID: %d\n", syscall(20));
+    debug(debug_sock, "PID: %d\n", syscall(20));
 
-    // ---- SCTP TEST ----
-    int sctp = sceNetSocket("sctp_test", AF_INET, SOCK_STREAM, IPPROTO_SCTP);
+    int raw6_sock = sceNetSocket("raw_icmp6", AF_INET6, SOCK_RAW, IPPROTO_ICMPV6);
 
-    if (sctp < 0) {
-        debug(sock, "SCTP not supported (error: %d)\n", sctp);
+    if (raw6_sock < 0) {
+        debug(debug_sock, "Raw ICMPv6 socket failed: %d\n", raw6_sock);
+        goto cleanup;
+    }
+
+    debug(debug_sock, "Raw ICMPv6 socket created: %d\n", raw6_sock);
+
+    struct sockaddr_in6 dest;
+    memset(&dest, 0, sizeof(dest));
+    dest.sin6_len = sizeof(dest);
+    dest.sin6_family = AF_INET6;
+    dest.sin6_port = 0;
+
+    const char *target_ip = "::1";
+    sceNetInetPton(AF_INET6, target_ip, &dest.sin6_addr);
+
+    uint8_t icmp6_packet[128];
+    memset(icmp6_packet, 0, sizeof(icmp6_packet));
+
+    icmp6_packet[0] = 2;
+    icmp6_packet[1] = 0;
+
+    uint32_t malformed_mtu = 0x00000000;
+    memcpy(icmp6_packet + 4, &malformed_mtu, 4);
+
+    uint8_t *quoted = icmp6_packet + 8;
+
+    quoted[0]  = 0x60;
+    quoted[1]  = 0x00;
+    quoted[2]  = 0x00; quoted[3] = 0x00;
+    quoted[4]  = 0x00; quoted[5] = 0x10;
+    quoted[6]  = 6;
+    quoted[7]  = 64;
+
+    memset(quoted + 8,  0, 16); quoted[8+15]  = 1;
+    memcpy(quoted + 24, &dest.sin6_addr, 16);
+
+    memset(quoted + 40, 0xAA, 16);
+
+    int packet_len = 8 + 40 + 16;
+
+    int sent = sceNetSendto(raw6_sock, icmp6_packet, packet_len, 0,
+                            (struct sockaddr *)&dest, sizeof(dest));
+
+    if (sent < 0) {
+        debug(debug_sock, "Sendto failed: %d\n", sent);
     } else {
-        debug(sock, "SCTP supported\n");
-        sceNetSocketClose(sctp);
+        debug(debug_sock, "Sent malformed ICMPv6: %d bytes\n", sent);
     }
-    // -------------------
 
-    sceNetSocketClose(sock);
+    sceNetSocketClose(raw6_sock);
+
+cleanup:
+    sceNetSocketClose(debug_sock);
 
     return 0;
 }
