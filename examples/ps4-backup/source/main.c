e78d219eae6578eb148b77d0da2673fd355f2302
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2026-03-04 15:13:07 +0100

    Update main.c

--- examples/ps4-backup/source/main.c
+++ examples/ps4-backup/source/main.c
@@ -1,8 +1,27 @@
 #include "ps4.h"
 
-#ifndef IPPROTO_ICMPV6
-#define IPPROTO_ICMPV6 58
-#endif
+#define AF_INET         2
+#define AF_INET6        23
+
+#define SOCK_STREAM     1
+#define SOCK_DGRAM      2
+#define SOCK_RAW        3
+
+#define IPPROTO_ICMP    1
+#define IPPROTO_ICMPV6  58
+
+struct in6_addr {
+    unsigned char s6_addr[16];
+};
+
+struct sockaddr_in6 {
+    uint8_t         sin6_len;
+    uint8_t         sin6_family;
+    uint16_t        sin6_port;
+    uint32_t        sin6_flowinfo;
+    struct in6_addr sin6_addr;
+    uint32_t        sin6_scope_id;
+};
 
 #define debug(sock, format, ...)                    \
     do {                                            \
@@ -27,8 +46,9 @@ int _main(void) {
     memset(server.sin_zero, 0, sizeof(server.sin_zero));
 
     int debug_sock = sceNetSocket(socketName, AF_INET, SOCK_STREAM, 0);
-    if (debug_sock < 0)
+    if (debug_sock < 0) {
         return 0;
+    }
 
     if (sceNetConnect(debug_sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
         sceNetSocketClose(debug_sock);
@@ -40,20 +60,20 @@ int _main(void) {
     int raw6_sock = sceNetSocket("raw_icmp6", AF_INET6, SOCK_RAW, IPPROTO_ICMPV6);
 
     if (raw6_sock < 0) {
-        debug(debug_sock, "Raw ICMPv6 socket failed: %d\n", raw6_sock);
+        debug(debug_sock, "Failed to create raw ICMPv6 socket: %d\n", raw6_sock);
         goto cleanup;
     }
 
-    debug(debug_sock, "Raw ICMPv6 socket created: %d\n", raw6_sock);
+    debug(debug_sock, "Raw ICMPv6 socket created successfully: %d\n", raw6_sock);
 
     struct sockaddr_in6 dest;
     memset(&dest, 0, sizeof(dest));
-    dest.sin6_len = sizeof(dest);
-    dest.sin6_family = AF_INET6;
-    dest.sin6_port = 0;
+    dest.sin6_len     = sizeof(dest);
+    dest.sin6_family  = AF_INET6;
+    dest.sin6_port    = 0;
 
     const char *target_ip = "::1";
-    sceNetInetPton(AF_INET6, target_ip, &dest.sin6_addr);
+    sceNetInetPton(AF_INET6, target_ip, &dest.sin6_addr.s6_addr);
 
     uint8_t icmp6_packet[128];
     memset(icmp6_packet, 0, sizeof(icmp6_packet));
@@ -66,15 +86,17 @@ int _main(void) {
 
     uint8_t *quoted = icmp6_packet + 8;
 
-    quoted[0]  = 0x60;
-    quoted[1]  = 0x00;
-    quoted[2]  = 0x00; quoted[3] = 0x00;
-    quoted[4]  = 0x00; quoted[5] = 0x10;
-    quoted[6]  = 6;
-    quoted[7]  = 64;
+    quoted[0] = 0x60;
+    quoted[1] = 0x00;
+    quoted[2] = 0x00; quoted[3] = 0x00;
+    quoted[4] = 0x00; quoted[5] = 0x10;
+    quoted[6] = 6;
+    quoted[7] = 64;
 
-    memset(quoted + 8,  0, 16); quoted[8+15]  = 1;
-    memcpy(quoted + 24, &dest.sin6_addr, 16);
+    memset(quoted + 8, 0, 16);
+    quoted[8 + 15] = 1;
+
+    memcpy(quoted + 24, &dest.sin6_addr.s6_addr, 16);
 
     memset(quoted + 40, 0xAA, 16);
 
@@ -84,9 +106,9 @@ int _main(void) {
                             (struct sockaddr *)&dest, sizeof(dest));
 
     if (sent < 0) {
-        debug(debug_sock, "Sendto failed: %d\n", sent);
+        debug(debug_sock, "sceNetSendto failed: %d\n", sent);
     } else {
-        debug(debug_sock, "Sent malformed ICMPv6: %d bytes\n", sent);
+        debug(debug_sock, "Malformed ICMPv6 Packet Too Big sent: %d bytes\n", sent);
     }
 
     sceNetSocketClose(raw6_sock);
