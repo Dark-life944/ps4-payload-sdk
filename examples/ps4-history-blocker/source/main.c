924bf2b4853c0256fad8738f63c2ee370984e384
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2026-03-10 06:02:12 +0100

    Update main.c

--- examples/ps4-history-blocker/source/main.c
+++ examples/ps4-history-blocker/source/main.c
@@ -1,5 +1,7 @@
 #include "ps4.h"
 
+#include <stdarg.h>
+
 typedef struct {
     uint64_t bits[16];
 } cpuset_t;
@@ -25,7 +27,7 @@ struct cmsghdr {
 
 #define ITERATIONS 2000000
 #define CONTROL_LEN 512
-#define DEBUG_IP "192.168.100.16"
+#define DEBUG_IP "192.168.176.162"
 #define DEBUG_PORT 9023
 
 struct cmsghdr *cmsg;
